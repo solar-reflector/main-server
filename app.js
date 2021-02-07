8aba2d6170858ed9c633ab2fbff6dee795453bdf
Scott Vincent <svincen1unb.ca>
2021-02-07 18:10:06 -0400

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+//SIO
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var windSpeed = 0;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -12,7 +17,18 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/page1.html'));
 })
 
+//SIO
+io.on('connection', function(client) {
+    console.log('Client connected...');
+
+    client.on('clicked', function(data) {
+	     windSpeed++;
+	     //send a message to all connected clients
+	     io.emit('windUpdate', windSpeed);
+     });
+});
+
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
