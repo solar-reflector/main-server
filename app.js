e3ed66c52d14b9cef5b67b5d1835aed852ad822d
Scott Vincent <svincen1unb.ca>
2021-02-26 20:25:04 -0400

    update

--- app.js
+++ app.js
@@ -49,29 +49,6 @@ wss.on('connection', function connection(ws, req) {
     });
 });
 
-// Weather request
-var options = {
-  host: 'api.openweathermap.org',
-  path: '/data/2.5/onecall?lat=45.964993&lon=-66.646332&exclude=minutely,hourly,alerts&units=metric&appid=b8ae12dff762333b98e1972f5ca1fc82'
-};
-
-callback = function(response) {
-  var weatherData = '';
-
-  //another chunk of data has been received, so append it to `str`
-  response.on('data', function (chunk) {
-    weatherData += chunk;
-  });
-
-  //the whole response has been received, so we just print it out here
-  response.on('end', function () {
-    console.log(weatherData);
-  });
-}
-
-http.request(options, callback).end(); // End of weather request
-
-
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
--- public/weather.js
+++ public/weather.js
@@ -1,1 +1,21 @@
+// Weather request
+var options = {
+  host: 'api.openweathermap.org',
+  path: '/data/2.5/onecall?lat=45.964993&lon=-66.646332&exclude=minutely,hourly,alerts&units=metric&appid=b8ae12dff762333b98e1972f5ca1fc82'
+};
 
+callback = function(response) {
+  var weatherData = '';
+
+  //another chunk of data has been received, so append it to `str`
+  response.on('data', function (chunk) {
+    weatherData += chunk;
+  });
+
+  //the whole response has been received, so we just print it out here
+  response.on('end', function () {
+    console.log(weatherData);
+  });
+}
+
+http.request(options, callback).end(); // End of weather request
